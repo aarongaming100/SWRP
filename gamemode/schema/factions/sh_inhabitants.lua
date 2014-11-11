29599b841c43319e02c8de0548f7baa8283c985a
aarongaming100 <aaroncaulfield912@gmail.com>
2014-11-11 19:41:01 +0000

    Added base factions no model change from default

---
+++ gamemode/schema/factions/sh_inhabitants.lua
@@ -0,0 +1,11 @@
+-- The 'nice' name of the faction.
+FACTION.name = "Inhabitants"
+-- A description used in tooltips in various menus.
+FACTION.desc = "The civilian's in the empire."
+-- A color to distinguish factions from others, used for stuff such as
+-- name color in OOC chat.
+FACTION.color = Color(20, 150, 15)
+
+-- FACTION.index is defined when the faction is registered and is just a numeric ID.
+-- Here, we create a global variable for easier reference to the ID.
+FACTION_INHABITANTS = FACTION.index
